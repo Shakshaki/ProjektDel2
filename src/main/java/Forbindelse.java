c6cd4231cc888e6ea38f6516ee77fcf0b4470f98
Jasmin <s170720@student.dtu.dk>
2019-11-06 10:26:26 +0100

    v5

--- src/main/java/Forbindelse.java
+++ src/main/java/Forbindelse.java
@@ -5,25 +5,25 @@ public class Forbindelse {
     public static void main(String[] args) { //psvm
         Forbindelse forbindelse = new Forbindelse();
         // forbindelse.updateConnection("192.168.239.24","root","groot");
-        forbindelse.searchUser( "0");
+        forbindelse.searchUser("0");
 
     }
 
     private Connection connection;
     private Statement stmt;
     private String url, userName, password;
-    private ResultSet userset,appointmentset;
+    private ResultSet userset, appointmentset;
     private String cpr;
 
 
-    public void updateConnection(String newUrl, String newUsername,String newPassword) {
+    public void updateConnection(String newUrl, String newUsername, String newPassword) {
 
-        url = "jdbc:mysql://"+newUrl+":3306/?user="+newUsername;
+        url = "jdbc:mysql://" + newUrl + ":3306/?user=" + newUsername;
         userName = newUsername;
         password = newPassword;
 
         try {
-            connection=DriverManager.getConnection(url,userName,password);
+            connection = DriverManager.getConnection(url, userName, password);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -45,26 +45,29 @@ public class Forbindelse {
             assert connection != null;
             stmt = connection.createStatement();
 
-            } catch(Exception e){
-                System.out.println("Forbindelse undtagelse: " + e.getMessage());
-                e.printStackTrace();
-            }
+        } catch (Exception e) {
+            System.out.println("Forbindelse undtagelse: " + e.getMessage());
+            e.printStackTrace();
         }
+    }
 
-    public void searchUser(String cpr){
+    public void searchUser(String cpr) {
 
         try {
-            userset = stmt.executeQuery("SELECT* FROM sund.person where cpr="+cpr+";");
-            int output = userset.getInt("cpr");
-            System.out.println("Bruger fundet med cpr:"+output);
+            userset = stmt.executeQuery("SELECT* FROM sund.person where cpr=" + cpr + ";");
+            if (userset.next()) {
+                int output = userset.getInt("cpr");
+                System.out.println("Bruger fundet med cpr:" + output);
+            }
 
-        } catch (Exception e){
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
-    public void searchAppointment(){
+    public void searchAppointment() {
 
 
     }
